9f1bcafc78cabd821a9e0f1cdb9e63c7c04a6791
Oleg Pukhaev <nuba8184@gmail.com>
2018-08-14 09:24:29 +0600

    Rename folder Sandbox to Components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Sandbox/App';//рабочая запутанная версия
+import App from './Components/App';
 
 import registerServiceWorker from './registerServiceWorker';
 
