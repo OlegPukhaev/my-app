11bf9c65575fd7bddd29858f65c239a760f903df
Oleg Pukhaev <nuba8184@gmail.com>
2018-08-14 11:31:01 +0600

    redux installing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
 import App from './Components/App';
-
 import registerServiceWorker from './registerServiceWorker';
+import allReducers from './reducers';
+
+const store = createStore (allReducers);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
